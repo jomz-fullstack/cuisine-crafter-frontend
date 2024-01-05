8fa12938c13ca95bc52808d36add5ae2a2aa62af
LEARN Academy <learnacademy@MacBook-Air-6.local>
2024-01-05 10:45:37 -0800

    frontend structure completed with routes

---
+++ src/mockReviews.js
@@ -0,0 +1,23 @@
+let mockReviews = [
+    {
+        header:"Taste delicious",
+        body:"the pasta was very creamy",
+        stars:3,
+        recipe_id:1,
+
+    },
+    {
+        header:"It was not that great",
+        body:"the steak was over cooked",
+        stars:1,
+        recipe_id:2,
+    },
+    {
+        header:"Best food ive had in a long time",
+        body:"would come back again!",
+        stars:5,
+        recipe_id:3,
+    },
+]
+
+export default mockReviews
