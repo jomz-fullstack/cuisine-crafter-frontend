b2b4fa178e54f4f648f6f105e991ea336976d679
LEARN Academy <learnacademy@MacBook-Air-6.local>
2024-01-08 10:54:18 -0800

    updated img size

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -9,10 +9,10 @@ const Index = ({ recipe }) => {
           <div key={recipeItem.id}>
             <Link to={`/show/${recipeItem.id}`}><h1>{recipeItem.name}</h1></Link>
             <br/>
+            <img src={recipeItem.image} alt={recipeItem.name} style={{ height: "500px", width: "500px"}}/>
+            <br/>
             <Link to={`/reviews/${recipeItem.id}`}>View Reviews</Link>
-            <p>{recipeItem.image}</p>
-            <p>Ingredients: {recipeItem.ingredients}</p>
-            <p>{recipeItem.protein}</p>
+            <p>Ingredients: {recipeItem.protein},{" "}{recipeItem.ingredients}</p>
             <p>Instructions: {recipeItem.instructions}</p>
             <p>{recipeItem.nutrition}</p>
           </div>
