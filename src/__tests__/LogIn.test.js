c77eb920787da733986d4104787bb844bc406054
LEARN Academy <learnacademy@MacBook-Air-6.local>
2024-01-17 15:11:04 -0800

    added testing to login

--- src/__tests__/LogIn.test.js
+++ src/__tests__/LogIn.test.js
@@ -1,16 +1,47 @@
-import { BrowserRouter } from "react-router-dom"
-import { render, screen } from "@testing-library/react"
-import LogIn from "../pages/LogIn"
+import { BrowserRouter } from "react-router-dom";
+import { render, screen, fireEvent, within } from "@testing-library/react";
+import LogIn from "../pages/LogIn";
 
 describe("<LogIn />", () => {
-    const renderHome = () =>
-      render(
-        <BrowserRouter>
-          <LogIn />
-        </BrowserRouter>
-      )
-    it("renders without crashing", () => {
-      renderHome()
-    //   screen.logTestingPlaygroundURL()
-    })
-})
+  const renderLogin = () =>
+    render(
+      <BrowserRouter>
+        <LogIn />
+      </BrowserRouter>
+    );
+
+  it("renders without crashing", () => {
+    renderLogin();
+    screen.logTestingPlaygroundURL();
+  });
+
+  it("renders 'No login? Sign up today!' heading", () => {
+    renderLogin();
+    expect(
+      screen.getByRole("heading", {
+        name: /no login\? sign up today!/i,
+      })
+    ).toBeInTheDocument();
+  });
+
+  it("renders 'Login' button", () => {
+    renderLogin();
+    expect(screen.getByRole('button', { name: /login/i })).toBeInTheDocument();
+  });
+
+  it("renders password input box", () => {
+    renderLogin();
+    const views = screen.getAllByText(/email: password:/i);
+    views.forEach(view => {
+      within(view).getByRole('textbox');
+    });
+  });
+  it("renders confirm password input box", () => {
+    renderLogin()
+    expect(screen.getByPlaceholderText(/confirm password/i))
+  })
+  it("renders both password and confirm", () => {
+    renderLogin()
+    expect(screen.getByText(/email: password: confirm password:/i))
+  })
+});
