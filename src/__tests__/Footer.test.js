796cf02be80ffbbe10690a0760234652d17065c4
LEARN Academy <learnacademy@MacBook-Air-6.local>
2024-01-17 14:30:37 -0800

    updated footer test

--- src/__tests__/Footer.test.js
+++ src/__tests__/Footer.test.js
@@ -1,23 +1,16 @@
-import { BrowserRouter } from "react-router-dom"
-import { render, screen } from "@testing-library/react"
-import Footer from "../components/Footer"
+import { BrowserRouter } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+import Footer from "../components/Footer";
 
 describe("<Footer />", () => {
-  const renderFooter = () =>
+  const renderHome = () =>
     render(
       <BrowserRouter>
         <Footer />
       </BrowserRouter>
-    )
-  it("renders without crashing", () => {
-    renderFooter()
-  })
-  it("renders the footer text", () => {
-    renderFooter()
-
-    const footerText = screen.getByText(/© 2023 Cuisine Crafter \| JOMZ/i, {
-      selector: "p",
+    );
+    it("renders cuisine crafter", () => {
+      renderHome()
+      expect(screen.getByText(/© 2023 cuisine crafter \|/i))
     })
-    expect(footerText).toBeInTheDocument()
-  })
-})
+});
